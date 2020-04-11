1d7035b12f4fd2403e6cccdfa4d385ad798c9bc7
zheuswalker <thndrwrth@gmail.com>
2020-04-11 17:55:13 +0800

    change root view

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('sample');
+    return view('addpatient');
 });
 Route::get('/addpatient', function () {
     return view('addpatient');
